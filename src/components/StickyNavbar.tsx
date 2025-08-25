54d1fd3879c7eb689ede95d663a1cee4e37d2c83
DEadMan10sds <alejandroadan.sds@gmail.com>
2025-08-24 19:23:52 -0600

    ADD: Updated english CV

--- src/components/StickyNavbar.tsx
+++ src/components/StickyNavbar.tsx
@@ -5,9 +5,8 @@ import {
   Typography,
   IconButton,
 } from "@material-tailwind/react";
-//import { useTranslation } from "react-i18next";
 import { ChangeLanguage } from "./ChangeLanguage";
-import { useTranslation } from "react-i18next";
+//import { useTranslation } from "react-i18next";
 
 const links = [
   {
@@ -22,7 +21,7 @@ const links = [
 
 export function StickyNavbar() {
   const [openNav, setOpenNav] = useState(false);
-  const { i18n } = useTranslation();
+  //const { i18n } = useTranslation();
   useEffect(() => {
     window.addEventListener(
       "resize",
@@ -49,7 +48,8 @@ export function StickyNavbar() {
       ))}
       <Typography as="li" color="blue-gray" className="p-1 font-normal">
         <a
-          href={`CV_${i18n.language.toUpperCase()}_-_AdanAlejandroSanchez.pdf`}
+          href={`CV_EN_-_AdanAlejandroSanchez.pdf`}
+          // href={`CV_${i18n.language.toUpperCase()}_-_AdanAlejandroSanchez.pdf`}
           className="flex items-center text-md font-semibold"
         >
           CV
